a68c9cea13aff555b166abbd94205809d0fd3a05
ninolis <97913013+ninolis@users.noreply.github.com>
2022-11-18 17:37:57 +0200

    Create 4-theory-autochecking.js

---
+++ js/4-theory-autochecking.js
@@ -0,0 +1,70 @@
+'use strict';
+
+///////////////////////////////////////////////
+// JS V2 block-4 Autochecking  ASSIGNMENT 00/48  Status: Done
+
+/*
+** THEORY
+
+
+** TASK
+
+
+** Test
+
+
+ */
+
+// Javascript Editor:
+// START
+/*
+
+*/
+
+// Javascript Editor:
+// FINISH
+/*
+
+console.log(
+*/
+
+// RESULT
+/*
+
+*/
+
+
+
+
+///////////////////////////////////////////////
+// JS V2 block-4 Autochecking  ASSIGNMENT 1/48  Status: Done
+
+/*
+** THEORY
+
+
+** TASK
+
+
+** Test
+
+
+ */
+
+// Javascript Editor:
+// START
+/*
+
+*/
+
+// Javascript Editor:
+// FINISH
+/*
+
+console.log(
+*/
+
+// RESULT
+/*
+
+*/
