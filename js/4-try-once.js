88c2c514df67fb0ba09632db5e6ca745ac76d68a
ninolis <97913013+ninolis@users.noreply.github.com>
2022-11-23 09:59:33 +0200

    Update 4-try-once.js

--- js/4-try-once.js
+++ js/4-try-once.js
@@ -1,11 +1,166 @@
 'use strict';
 
+/*
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: 'Mango', score: 83 },
+  { name: 'Poly', score: 59 },
+  { name: 'Ajax', score: 37 },
+  { name: 'Kiwi', score: 94 },
+  { name: 'Houston', score: 64 },
+];
+
+const best = students.filter((student) => student.score >= HIGH_SCORE);
+console.log(best); // Array of objects named Mango and Kiwi
+*/
+
+/*
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
+
+// const getUserNames = (users) => users.map((user) => user.name);
+// const getUserEmails = (users) => users.map((user) => user.email);
+
+// console.log(getUserNames(users));
+// console.log(getUserEmails(users));
+
+// const getUsersWithEyeColor = (users, color) =>
+//   users.filter((user) => user.eyeColor === color);
+
+// console.log(getUsersWithEyeColor(users, 'blue'));
+// console.log(getUsersWithEyeColor(users, 'green'));
+// console.log(getUsersWithEyeColor(users, 'brown'));
+
+// const getUsersWithAge = (users, minAge, maxAge) =>
+//   users.filter((user) => user.age >= minAge && user.age <= maxAge);
+
+// console.log(getUsersWithAge(users, 20, 30));
+// console.log(getUsersWithAge(users, 30, 40));
+// console.log(getUsersWithAge(users, 80, 100));
+
+const getUsersWithFriend = (users, friendName) =>
+  users.filter((user) => user.friends.includes(friendName));
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+console.log(getUsersWithFriend(users, 'Adrian Cross'));
+
+*/
+
+/*
+function changeEven(numbers, value) {
+  // Change code below this line
+  const newArray = [];
+
+  numbers.forEach((element) => {
+    if (element % 2 === 0) {
+      newArray.push(element + value);
+    } else {
+      newArray.push(element);
+    }
+  });
+
+  return newArray;
+  // Change code above this line
+}
+
+changeEven([1, 2, 3, 4, 5], 10); // returns a new array [1, 12, 3, 14, 5]
+changeEven([2, 8, 3, 7, 4, 6], 10); // returns a new array [12, 18, 3, 7, 14, 16]
+changeEven([17, 24, 68, 31, 42], 100); // returns a new array [17, 124, 168, 31, 142]
+changeEven([44, 13, 81, 92, 36, 54], 100); // returns a new array [144, 13, 81, 192, 136, 154]
+
+console.log(changeEven([1, 2, 3, 4, 5], 10)); // returns a new array [1, 12, 3, 14, 5]
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); // returns a new array [12, 18, 3, 7, 14, 16]
+console.log(changeEven([17, 24, 68, 31, 42], 100)); // returns a new array [17, 124, 168, 31, 142]
+console.log(changeEven([44, 13, 81, 92, 36, 54], 100)); // returns a new array [144, 13, 81, 192, 136, 154]
+*/
+
+/*
 // Change code below this line
 const calculateTotalPrice = (orderedItems) => {
   let totalPrice = 0;
   orderedItems.forEach((item) => (totalPrice += item));
   return totalPrice;
 };
+*/
 
 /*
 function getCommonElements(firstArray, secondArray) {
